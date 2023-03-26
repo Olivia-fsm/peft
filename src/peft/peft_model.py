86de09446af4e0fd6c323f92d6318cda2fac92b7
Olivia Slmin <slmin@tsf-452-wpa-1-162.epfl.ch>
2023-03-26 19:12:23 +0200

    init-from-string-correct

--- src/peft/peft_model.py
+++ src/peft/peft_model.py
@@ -252,6 +252,7 @@ class PeftModel(PushToHubMixin, torch.nn.Module):
             past_key_values = past_key_values.permute([2, 0, 3, 1, 4]).split(
                 self.peft_config.num_transformer_submodules * 2
             )
+            print('past_key_values.shape: ', past_key_values.shape)
             if TRANSFORMERS_MODELS_TO_PREFIX_TUNING_POSTPROCESS_MAPPING.get(self.config.model_type, None) is not None:
                 post_process_fn = TRANSFORMERS_MODELS_TO_PREFIX_TUNING_POSTPROCESS_MAPPING[self.config.model_type]
                 past_key_values = post_process_fn(past_key_values)
